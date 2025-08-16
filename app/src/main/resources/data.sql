8dda82928a85f0095006b9ff1dd216a26ba7ba5e
dsouzajm <danielsouza.jm@gmail.com>
2025-08-16 19:53:13 -0300

    Criacao da tabela de pessoas.

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -1,2 +1,8 @@
-INSERT  INTO persons(name) VALUES('Amila');
-INSERT  INTO persons(name) VALUES('Iroshan');
+INSERT INTO pessoas (id, apelido, nome, nascimento, stack)
+VALUES (
+    'f7379ae8-8f9b-4cd5-8221-51efe19e721b',
+    'josé',
+    'José Roberto',
+    '2000-10-01',
+    ARRAY['C#', 'Node', 'Oracle']
+);
--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,5 +1,8 @@
-CREATE TABLE IF NOT EXISTS persons(
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+-- Criação da tabela para armazenar dados de pessoas
+CREATE TABLE pessoas (
+    id UUID PRIMARY KEY,
+    apelido VARCHAR(32) UNIQUE NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    nascimento DATE NOT NULL,
+    stack TEXT[]
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
