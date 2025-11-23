4e8ea1ac51f9804287d5793be8fdb1f6872e8318
dsouzajm <danielsouza.jm@gmail.com>
2025-11-23 20:24:42 -0300

    Retirada de imports.

--- src/main/java/br/com/dsouzajm/controller/PessoaController.java
+++ src/main/java/br/com/dsouzajm/controller/PessoaController.java
@@ -8,7 +8,6 @@ import br.com.dsouzajm.utils.PessoaUtils;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
